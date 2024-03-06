95e63e55de10b497546ebd2dd38adc30f6c9a4df
Hasham Asad <tipuasad2001@gmail.com>
2024-03-07 02:20:47 +0500

    timeSlot

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Appointment from './component/Appointment';
 import AppointmentCalendar from './component/AppoinmentCalendar';
+import TimeSlot from './component/TimeSlot';
 
 function App() {
   return (
     <div className="App">
+      <TimeSlot />
      <AppointmentCalendar />
      <Appointment />
     </div>
---
+++ src/component/TimeSlot.jsx
@@ -0,0 +1,12 @@
+import ReactTimeslotCalendar from 'react-timeslot-calendar'
+
+function TimeSlot(){
+
+    return (
+        <>
+        <ReactTimeslotCalendar />
+        </>
+    )
+}
+
+export default TimeSlot
