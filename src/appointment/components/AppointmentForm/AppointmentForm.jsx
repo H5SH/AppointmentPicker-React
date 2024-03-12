4c534cec5c251299985865b65678afc14693f368
Hasham Asad <tipuasad2001@gmail.com>
2024-03-12 20:32:41 +0500

    formAndStylingStillNotFunctional

--- src/appointment/components/AppointmentForm/AppointmentForm.jsx
+++ src/appointment/components/AppointmentForm/AppointmentForm.jsx
@@ -1,11 +1,68 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { setDataContext } from "../../../../utilities/SettingContext"
 import { days, months } from "../data"
 import { GotoBackButton } from "../../../../utilities/SubmitButton"
+import { Field, Formik, Form, ErrorMessage } from "formik"
+import { initialValues, reasonsForVisit, validation } from "../__request/initialValuesAndValidation"
+import { addAppointmentReq } from "../__request/appointmentRequest"
+import Select from 'react-select'
+import { getFacilitiesDataReq } from "../../../organization/components/facilities/__request/RequestFacility"
+import { getProviderReq } from "../../../providers/__request/RequestProvider"
+import { getPatientDataReq } from "../../../patients/__request/RequestPatient"
+import { pad } from "../../../../utilities/Helper"
 
 
 function AppointmentForm() {
     const { appointmentDate, appointmentTime, appointmentDay, appointmentProvider } = useContext(setDataContext)
+    const [ hours, min ] = appointmentTime.split(':')
+    const [facilities, setFacilities] = useState([])
+    const [providers, setProviders] = useState([])
+    const [patients, setPatients] = useState([])
+
+    async function getPatientData(){
+        const response = await getPatientDataReq(0, { text: '', field: 'first_name' })
+        setPatients(response.data.data)
+    }
+
+    async function getFacilitiesData() {
+        const response = await getFacilitiesDataReq()
+        setFacilities(response.data.data)
+    }
+
+    async function getProvider() {
+        const response = await getProviderReq('rendering', 0, { text: '', field: 'first_name' })
+        setProviders(response.data.data)
+    }
+
+    async function addAppointment(values) {
+        console.log('addAppointment')
+        addAppointmentReq(values)
+    }
+
+    function validate(){
+        return true
+    }
+
+    console.log(`${pad(hours)}:${min}`, 'Hasham')
+
+    const customLabelFacilities = ({ name }) => (
+        <div style={{ color: "black", }}>
+            {name}
+        </div>
+    )
+
+    const customLabelProvider = ({ full_name }) => (
+        <div>
+            {full_name}
+        </div>
+    )
+
+    useEffect(() => {
+        getFacilitiesData()
+        getProvider()
+        getPatientData()
+    }, [])
+
 
     return (
         <div
@@ -31,10 +88,208 @@ function AppointmentForm() {
                         data-kt-scroll-dependencies='#kt_appoitment_header, #kt_appointment_footer'
                         data-kt-scroll-offset='5px'
                     >
+                        <Formik
+                            initialValues={{
+                                ...initialValues,
+                                date: `${appointmentDate.getFullYear()}-${pad(appointmentDate.getMonth() + 1)}-${pad(appointmentDate.getDate())}`,
+                                time: `${pad(hours)}:${min}`,
+                                visit_length: '15',
+                            }}
+                            validationSchema={validation}
+                            onSubmit={(values) => {
+                                addAppointment(values)
+                            }}
+                            enableReinitialize
+                        >
+                            {({ handleSubmit, setValues, values }) => (
+                                <Form>
+                                    <div className="row">
+                                        <h2 className="pb-4">Patient Information</h2>
+                                        <div className="col-4">
+                                            <label className="form-label">Patient</label>
+                                            <Select
+                                            options={patients}
+                                            formatOptionLabel={customLabelProvider}
+                                            onChange={(patient)=> {
+                                                patient ?
+                                                setValues({
+                                                    ...values,
+                                                    patient: patient || '',
+                                                    first_name: patient.first_name || '',
+                                                    last_name: patient.last_name || '',
+                                                    dob: patient.dob || '',
+                                                    insurance_name: patient.insurance_name || '',
+                                                    phone: patient.hphone || ''
+                                                }): setValues({
+                                                    ...values, 
+                                                    patient: '',
+                                                    first_name: '',
+                                                    last_name: '',
+                                                    dob: '',
+                                                    insurance_name:'',
+                                                    phone:  ''
+                                                })
+                                            }}
+                                            value={values.patient}
+                                            getOptionValue={(option)=> option.full_name}
+                                            isSearchable
+                                            isClearable 
+                                            />
+                                        </div>
+                                        <div className="col-3">
+                                            <label className="form-label">First Name</label>
+                                            <Field type='text' name='first_name' className='form-control' disabled={validate()}/>
+                                        </div>
+                                        <div className="col-3">
+                                            <label className="form-label">Last Name</label>
+                                            <Field type='text' name='last_name' className="form-control" disabled={validate()}/>
+                                        </div>
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">Date Of Birth</label>
+                                            <Field type='text' name='dob' className="form-control" disabled={validate()}/>
+                                        </div>
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">Phone</label>
+                                            <Field type='text' name='phone' className="form-control" disabled={validate()}/>
+                                        </div>
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">Health Plan Eligibility Name</label>
+                                            <Field type='text' name='health_paln_Eligibility_name' className="form-control" disabled={validate()}/>
+                                        </div>
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">Insurance Name</label>
+                                            <Field type='text' name='insurance_name' className="form-control" disabled={validate()}/>
+                                        </div>
+                                    </div>
+                                    <div className="row pt-4">
+                                        <h2 className="pt-4 pb-4">Appointment Information</h2>
+                                        <div className="col-4">
+                                            <label className="form-label required">Office Name</label>
+                                            <Select
+                                                options={facilities}
+                                                formatOptionLabel={customLabelFacilities}
+                                                onChange={(facility) => {
+                                                    setValues({
+                                                        ...values,
+                                                        office_name: facility | ''
+                                                    })
+                                                }}
+                                                value={values.office_name}
+                                                getOptionValue={(option) => option.name}
+                                                style={{
+                                                    color: 'black',
+                                                }}
+                                                isSearchable
+                                                isClearable
+                                            />
+                                        </div>
 
-                        <h3>{`${appointmentDay} ${months[appointmentDate.getMonth()]} ${appointmentDate.getDate()} ${appointmentDate.getFullYear()}`}</h3>
+                                        <div className="col-4">
+                                            <label className="form-label required">With</label>
+                                            <Select
+                                                options={providers}
+                                                formatOptionLabel={customLabelProvider}
+                                                onChange={(provider) => {
+                                                    setValues({
+                                                        ...values,
+                                                        provider: provider | ''
+                                                    })
+                                                }}
+                                                value={values.provider}
+                                                getOptionLabel={(option) => option.full_name}
+                                                styles={{
+                                                    color: 'black'
+                                                }}
+                                                isSearchable
+                                                isClearable
+                                            />
+                                        </div>
+
+                                        <div className="col-4">
+                                            <label className="form-label">Resources</label>
+                                            <Select
+                                            />
+                                        </div>
+
+
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label required">Visit Reason</label>
+                                            <Select
+                                            options={reasonsForVisit}
+                                            formatOptionLabel={customLabelFacilities}
+                                            onChange={(reason)=> setValues({
+                                                ...values,
+                                                visit_reason: reason || ''
+                                            })}
+                                            value={values.visit_reason}
+                                            getOptionLabel={(option)=> option.name}
+                                            style={{
+                                                color: 'black'
+                                            }} 
+                                            isSearchable
+                                            isClearable
+                                            />
+                                        </div>
+
+
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">On({appointmentDay})</label>
+                                            <Field type='date' name='date' className='form-control' disabled={validate()}/>
+                                        </div>
+
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">At</label>
+                                            <Field type='time' name='time' className='form-control' disabled={validate()}/>
+                                        </div>
+
+                                        <div className="col-2 pt-4">
+                                            <label className="form-label">Visit Length</label>
+                                            <Field name='visit_length' type='number' className='form-control' disabled={validate()}/>
+                                        </div>
+
+                                        <div className="col-1 pt-4">
+                                            <label className="form-label">Color</label>
+                                            <Field name='color' type='color' className='form-control'/>
+                                        </div>
+
+
+                                        <div className="col-2 pt-4">
+                                            <label className="form-label">Repeat</label>
+                                            <Field name='repeat' type='number' className='form-control' disabled={validate()}/>
+                                        </div>
+
+                                        <div className="col-3 pt-4">
+                                            <label className="form-label">Days Between Visits</label>
+                                            <Field name='days_between_visits' type='number' className='form-control' disabled={validate()} />
+                                        </div>
+
+                                        <div className="col-4 pt-10 text-center">
+                                            <label className="form-check-label pe-2">In Office</label>
+                                            <input type="radio" name="in_office" className="form-check-input"/>
+                                            <label className="form-check-label ps-4 pe-2">Virtual Visit</label>
+                                            <input type="radio" name="in_office" className="form-check-input"/>
+                                        </div>
+
+                                        <div className="col-12">
+                                            <label className="form-label">Note</label>
+                                            <textarea value={values.comment} className="form-control" style={{height: '114px'}} onChange={(e)=>setValues({
+                                                ...values,
+                                                comment: e.target.value
+                                            })}></textarea>
+                                            <ErrorMessage>
+                                                {({ msg }) => <div>{msg}</div>}
+                                            </ErrorMessage>
+                                        </div>
+
+                                        {console.log(values.patient)}
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
+
+                        {/* <h3>{`${appointmentDay} ${months[appointmentDate.getMonth()]} ${appointmentDate.getDate()} ${appointmentDate.getFullYear()}`}</h3>
                         <h4>{`${appointmentTime}`}</h4>
-                        <h2>{appointmentProvider.full_name}</h2>
+                        <h2>{appointmentProvider.full_name}</h2> */}
                     </div>
                 </div>
                 <div
--- src/appointment/components/timeSlots/TimeSlotDay.jsx
+++ src/appointment/components/timeSlots/TimeSlotDay.jsx
@@ -4,24 +4,54 @@ import { pad } from '../../../../utilities/Helper'
 import './timeslots.css'
 import { setDataContext } from '../../../../utilities/SettingContext'
 
-function TimeSlotDay({ displayOnlyTime, displayOnlyHover = false, day, start = 8, end = 17 ,gap = 15, }) {
+function TimeSlotDay({ displayOnlyTime, displayOnlyHover = false, day, start = 8, end = 17, gap = 15, }) {
     const { setAppointmentTime, setAppointmentDay } = useContext(setDataContext)
     // times take start and end time and returns an array of hours
     const slots = times(start, end)
     const gaps = skips(gap)
 
     return (
-        <div className=''>
-            {slots.map((time, index) => (
+        <div>
+            {!displayOnlyHover && !displayOnlyTime &&
+                <div className='row'>
+                    <div className='col-1 bg-primary text-center border border-1'>
+                        Time
+                    </div>
+                    <div className='col-1 bg-primary border border-1'>
+                        Length
+                    </div>
+                    <div className='col-1 bg-primary text-center border border-1'>
+                        Patient Name
+                    </div>
+                    <div className='col-1 bg-primary text-center border border-1'>
+                        DOB
+                    </div>
+                    <div className='col-1 bg-primary text-center border border-1'>
+                        Home/Cell Phone
+                    </div>
+                    <div className='col-2 bg-primary text-center border border-1'>
+                        Provider/ Staff
+                    </div>
+                    <div className='col-2 bg-primary text-center border border-1'>
+                        Reason for Visit
+                    </div>
+                    <div className='col-1 bg-primary text-center border border-1'>
+                        Status
+                    </div>
+                    <div className='col-2 bg-primary text-center border border-1'>
+                        Automated Eligibility Status
+                    </div>
+                </div>}
+            {slots.map((time) => (
                 <>
-                    {gaps.map((gap) => (
+                    {gaps.map((gap, index) => (
                         <div key={index} className='row' onClick={() => {
                             setAppointmentTime(`${time}:${pad(gap)}`)
                             setAppointmentDay(day)
                         }} >
                             {!displayOnlyHover &&
-                                <div className={`border border-success bg-light-primary ${displayOnlyTime ? 'col':'col-1 text-center'}`}>
-                                  <u>{`${time}:${pad(gap)}`}</u>
+                                <div className={`border border-1 bg-white ${displayOnlyTime ? 'col' : 'col-1 text-center'}`}>
+                                    {`${time}:${pad(gap)}`}
                                 </div>
                             }
                             {!displayOnlyTime &&
